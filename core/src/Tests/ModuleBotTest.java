230c0cf13a1626f87d8a2e2968635cf2d7670aec
Artem Ilin <artem28147@yandex.ru>
2018-05-12 23:10:13 +0300

    travis file + test created

---
+++ core/src/Tests/ModuleBotTest.java
@@ -0,0 +1,13 @@
+package Tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ModuleBotTest {
+    @Test
+    public void test_1(){
+        Assert.assertEquals(1, 1);
+    }
+}
