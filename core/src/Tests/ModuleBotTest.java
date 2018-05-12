42cc842194fd5cc84c6d3b5356179f4d7f579497
Artem Ilin <artem28147@yandex.ru>
2018-05-12 23:21:34 +0300

    changed test to failing

--- core/src/Tests/ModuleBotTest.java
+++ core/src/Tests/ModuleBotTest.java
@@ -8,6 +8,6 @@ import static org.junit.Assert.*;
 public class ModuleBotTest {
     @Test
     public void test_1(){
-        Assert.assertEquals(1, 1);
+        Assert.assertEquals(2, 1);
     }
 }
